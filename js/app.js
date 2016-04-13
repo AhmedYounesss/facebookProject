06ed158207be0191deabd94514b0972e56e34d61
AhmedYounesss <smartwayappsteam@gmail.com>
2016-04-14 00:06:51 +0200

    Ajout la liste des amis avec ajustement de son css

--- js/app.js
+++ js/app.js
@@ -26,5 +26,31 @@ angular.module("myApp").config(function($stateProvider,$urlRouterProvider){
             controller: "profilCtrl"
         });
 
-});
+})
 
+.filter('searchFor', function(){
+
+//les filtres doivent retourner une fonction.
+    return function(arr, searchString){
+
+        if(!searchString){
+            return arr;
+        }
+
+        var result = [];
+
+        searchString = searchString.toLowerCase();
+
+        // Using the forEach helper method to loop through the array
+        angular.forEach(arr, function(item){
+
+            if(item.title.toLowerCase().indexOf(searchString) !== -1){
+                result.push(item);
+            }
+
+        });
+
+        return result;
+    };
+
+});
